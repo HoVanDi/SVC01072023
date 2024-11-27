ee0cb66bbb655fc7873b0854c775ec8d04ca02c8
HoVanDi <hovandideveloper@gmail.com>
2024-11-27 21:19:03 +0700

    defaul component

--- fe/sivishop/src/index.js
+++ fe/sivishop/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<h1>HoVandi</h1>
+<h1>
+    <HomePage />
+</h1>
 );
 
