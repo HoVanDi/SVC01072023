76df7af310b3ec805e251ffb2538b6ba6fb15ff8
HoVanDi <hovandideveloper@gmail.com>
2024-11-27 22:55:57 +0700

    Custom router

--- fe/sivishop/src/pages/users/homePage/index.js
+++ fe/sivishop/src/pages/users/homePage/index.js
@@ -3,9 +3,7 @@ import {memo} from 'react'
 const HomaPage = () => {
   return (
     <div>
-      <h1>Header</h1>
-      <h1>Body</h1>
-      <h1>Footer</h1>
+      <h1>HomaPage</h1>
     </div>
   );
 }
